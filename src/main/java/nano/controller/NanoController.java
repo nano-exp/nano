600d6acd2cd1ab6984fdd617c56b93b3d992f1ee
cbdyzj <cbdyzj@gmail.com>
2023-12-15 17:32:02 +0800

    Amend

--- src/main/java/nano/controller/NanoController.java
+++ src/main/java/nano/controller/NanoController.java
@@ -2,6 +2,7 @@ package nano.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class NanoController {
     @GetMapping("/hello")
     public ResponseEntity<?> hello() {
         return ResponseEntity.ok("hi");
+    }
+
+    @RequestMapping("/api/bark")
+    public ResponseEntity<?> bark() {
+        return ResponseEntity.ok("");
     }
 }
