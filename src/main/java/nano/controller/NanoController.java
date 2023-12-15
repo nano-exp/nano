4e37f0249c3118448481dcceba1ecf66fdca112c
cbdyzj <cbdyzj@gmail.com>
2023-12-15 17:38:02 +0800

    Amend

--- src/main/java/nano/controller/NanoController.java
+++ src/main/java/nano/controller/NanoController.java
@@ -1,10 +1,13 @@
 package nano.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class NanoController {
     @GetMapping("/hello")
@@ -13,7 +16,8 @@ public class NanoController {
     }
 
     @RequestMapping("/api/bark")
-    public ResponseEntity<?> bark() {
-        return ResponseEntity.ok("");
+    public ResponseEntity<?> bark(@RequestBody(required = false) String body) {
+        log.info("bark body: %s".formatted(body));
+        return ResponseEntity.ok(body);
     }
 }
