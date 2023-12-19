51242fe7cd21b38f4788d61df68795e8e48a24b7
cbdyzj <cbdyzj@gmail.com>
2023-12-19 13:35:07 +0800

    Amend

--- src/main/java/nano/service/CornService.java
+++ src/main/java/nano/service/CornService.java
@@ -11,7 +11,12 @@ public class CornService {
     private final BarkService barkService;
 
     @Scheduled(fixedDelay = 30 * 1000)
-    public void scheduledForBark() {
+    public void scheduledSendBarkMessage() {
         this.barkService.sendNoticeMessageNeeded();
+    }
+
+    @Scheduled(fixedDelay = 60 * 60 * 1000)
+    public void scheduledPersistBarkBase() {
+        this.barkService.persistStore();
     }
 }
