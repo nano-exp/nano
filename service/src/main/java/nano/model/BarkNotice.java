0571ad2b30526dc4b98b474db14a3284fb54c76c
cbdyzj <cbdyzj@gmail.com>
2024-01-05 19:39:58 +0800

    Amend

--- service/src/main/java/nano/model/BarkNotice.java
+++ service/src/main/java/nano/model/BarkNotice.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import nano.common.Env;
 import nano.common.Json;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.util.ObjectUtils;
 
 import java.net.URI;
 
@@ -18,18 +19,14 @@ public class BarkNotice {
     public static @NotNull String parseContent(@NotNull BarkMessage message) {
         var payload = message.getPayload();
         try {
-            var template = """
-                    ⚠️Message: [%s]
-                    
-                    %s
-                    
-                    %s
-                    """;
+            var template = "⚠️Message: [%s]%s\n\n%s\n\n%s";
             var parsed = Json.parseMap(payload);
             var id = message.getId();
-            var s = (String) parsed.getOrDefault("title", payload);
-            var link = URI.create(Env.NANO_HOST).resolve("/s/bark/m-%s".formatted(id));
-            return template.formatted(id, s, link);
+            var domain = message.getDomain();
+            var domainToAppend = ObjectUtils.isEmpty(domain) ? "" : " for " + domain;
+            var title = (String) parsed.getOrDefault("title", payload);
+            var link = URI.create(Env.NANO_HOST).resolve("/s/bark/m-" + id);
+            return template.formatted(id, domainToAppend, title, link);
         } catch (Exception ex) {
             return payload;
         }
