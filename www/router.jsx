b0f1328aa4f9d8839b969d0c50d0a3b7f3b6485f
cbdyzj <cbdyzj@gmail.com>
2024-04-17 20:57:15 +0800

    Amend

--- www/router.jsx
+++ www/router.jsx
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const Nano = () => import('./Nano.jsx')
+const BarkMessage = () => import('./bark/message/App.jsx')
+const NotFound = () => import('./common/NotFound.jsx')
+
 export default createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: () => import('./Nano.jsx') },
-        { path: '/bark/message', component: () => import('./bark/message/App.jsx') },
-        { path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import('./common/NotFound.jsx') },
+        { path: '/', component: Nano, },
+        { path: '/bark/message', component: BarkMessage, },
+        { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound, },
     ],
 })
