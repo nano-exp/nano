c7342476ecd126444400749f5f07adb50711ecb3
cbdyzj <cbdyzj@gmail.com>
2024-04-24 20:03:22 +0800

    Amend

--- www/bark/apis/getMessage.js
+++
@@ -1,4 +0,0 @@
-export  default async function getMessage(id) {
-    const response = await fetch(`/api/bark/message/${id}`)
-    return  await response.json()
-}
--- www/router.jsx
+++ www/router.jsx
@@ -1,14 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const Nano = () => import('./Nano.jsx')
-const BarkMessage = () => import('./bark/message/App.jsx')
 const NotFound = () => import('./common/NotFound.jsx')
 
 export default createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: Nano, },
-        { path: '/bark/message', component: BarkMessage, },
         { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound, },
     ],
 })
