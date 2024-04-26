0cd2ba55057ae22b8f3b2300b34965e4609f38b3
cbdyzj <cbdyzj@gmail.com>
2024-04-26 23:36:37 +0800

    Add ErrorAdvice

---
+++ service/src/main/java/nano/controller/ErrorAdvice.java
@@ -0,0 +1,14 @@
+package nano.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@ControllerAdvice
+public class ErrorAdvice {
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public String on404() {
+        return "forward:/";
+    }
+}
