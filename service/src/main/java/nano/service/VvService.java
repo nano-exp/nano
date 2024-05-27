7657fa23d7b47e3a496dbf11e6056266bdf4bcde
cbdyzj <cbdyzj@gmail.com>
2024-05-27 23:33:12 +0800

    Amend

--- service/src/main/java/nano/service/VvService.java
+++ service/src/main/java/nano/service/VvService.java
@@ -6,7 +6,10 @@ import nano.model.Vv;
 import nano.repository.VvRepository;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
@@ -19,6 +22,11 @@ public class VvService {
     public List<Vv> search(@NotNull final String keyword,
                            @NotNull final Integer pageIndex,
                            @NotNull final Integer pageSize) {
-        return this.vvRepository.findVvList(keyword, pageSize, (pageIndex - 1) * pageSize);
+        var l = this.vvRepository.findVvList(keyword, pageSize, (pageIndex - 1) * pageSize);
+        if (ObjectUtils.isEmpty(keyword)) {
+            l = new ArrayList<>(l);
+            Collections.shuffle(l);
+        }
+        return l;
     }
 }
