a0ae0fc5ed6f2662b48f4d0d3f92aab3074351e7
cbdyzj <cbdyzj@gmail.com>
2024-06-05 23:39:01 +0800

    Update Nano Exp

--- www/common/NotFound.jsx
+++ www/common/NotFound.jsx
@@ -1,9 +1,24 @@
 import { defineComponent } from 'vue'
+import { css } from '@emotion/css'
+
+const ClassName = css`
+    min-height: 100vh;
+    display: grid;
+    place-items: center;
+    font-size: 7rem;
+    font-weight: bold;
+`
+
+const emoji = ['😭', '🤔', '🤕', '😫', '😵', '🙏', '🫣', '🤪', '👀']
 
 export default defineComponent({
     render() {
+        const emo = emoji[emoji.length * Math.random() | 0]
         return (
-            <div>404</div>
+            <div class={ClassName}>
+                4️⃣0️⃣4️⃣
+                {emo}
+            </div>
         )
     },
 })
--- www/pages/Nano.jsx
+++ www/pages/Nano.jsx
@@ -1,5 +1,30 @@
 import { defineComponent } from 'vue'
 import { useAppStore } from '../store/app.js'
+import { css } from '@emotion/css'
+
+const ClassName = css`
+    .title {
+        position: relative;
+        text-align: center;
+        top: 25vh;
+        font-weight: 600;
+        letter-spacing: -0.5px;
+        font-size: 72px;
+    }
+
+    .title > span {
+        font-size: 72px;
+        background: linear-gradient(90deg, #f87171, #4ade80, #60a5fa);
+        -webkit-background-clip: text;
+        background-clip: text;
+        color: transparent;
+    }
+
+    .title > a {
+        text-decoration: none;
+    }
+
+`
 
 export default defineComponent({
     setup() {
@@ -11,7 +36,11 @@ export default defineComponent({
     render({ appStore }) {
         const { name } = appStore
         return (
-            <div>{name}</div>
+            <div class={ClassName}>
+                <div class="title">
+                    <span>{name}</span> <a href="https://github.com/nano-exp" target="_blank">🪄</a>
+                </div>
+            </div>
         )
     },
 })
